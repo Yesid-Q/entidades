149edf0e7699328336dbd021448cb02db3dbb2d1
Yesid Gonzalez <oquintero@openmytab.com>
2022-09-15 11:57:02 -0500

    feature: Se modifica el estandar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    loadChildren: () => import('./pages/list-entity/list-entity.module').then((m) => m.ListEntityModule)
+  },
+  {
+    path: 'table',
+    loadChildren: () => import('./pages/table-entity/table-entity.module').then((m) => m.TableEntityModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
